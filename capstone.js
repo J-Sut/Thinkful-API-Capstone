31421c873f2824b5efb007d20620ba1ec07bf400
j-Sut <flowstategame@gmail.com>
2017-03-30 23:47:59 -0700

    displaying flickr json text info

--- capstone.js
+++ capstone.js
@@ -1,5 +1,10 @@
+// 1) State 
+
+
+
 // example of flickr API call that returns data: 
 
+// 2) f(modify-State)
 
 //parts of the api request
 function getFlickrApiData(searchTerm, callback) {
@@ -17,16 +22,45 @@ function getFlickrApiData(searchTerm, callback) {
 	console.log("after .getJSON request");
 
 };
-
 function gotFlickrData(data) {
-	console.log(data);
+//*** Can enter into the object like this
+	
+	displayFlickrResults(data);
 };
 
 // format for displaying an image: 
 // https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
 
+// 3) f(render-State)
 
-$('h1').css("background-color", "darkgreen");
+function displayFlickrResults(data) {
+//*** Can enter into the object like this
+	console.log("from callback: " + data);
+	
+	let path = data.photos.photo;
+	console.log(path);
+
+
+	for (var i = 0; i <path.length; i++) {
+		let farmId = data.photos.photo[i].farm;
+		let serverId = data.photos.photo[i].server;
+		let imageId = data.photos.photo[i].id;
+		let secretId = data.photos.photo[i].secret;
+
+
+		$('#display-results').append( "<li>" + data.photos.photo[i].owner + "</li>");
+	}
+
+
+
+	
+	//console.log("inside displayFlickrResults");
+
+};
+
+
+
+// 4) Event Listeners
 
 $('#submit').on("click", function() {
 	console.log("submit button working");
